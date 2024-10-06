e64256cf9cc13f180c1ddd61e8b39866deca1c60
Darya Jenkins <melissegreenleaf@gmail.com>
2024-10-06 22:56:41 +0200

    bonusMiles

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int amount = 25_729;
+        int mile = 20;
+
+        int bonusMiles;
+        bonusMiles = amount / mile;
+
+        System.out.println(bonusMiles + " бонусных миль");
+    }
 }
